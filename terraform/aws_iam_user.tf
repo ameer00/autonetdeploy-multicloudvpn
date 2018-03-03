d3bad62b7cd831bc2a2186a437d64cc10e8f4e07
Ameer Abbas <ameer00@gmail.com>
2018-03-03 13:32:07 -0800

    Update aws_iam_user.tf

--- terraform/aws_iam_user.tf
+++ terraform/aws_iam_user.tf
@@ -18,6 +18,20 @@ variable "name" {
 
 variable "policy" {
   description = "The raw json policy"
+  default = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [{
+      "Effect": "Allow",
+      "Action": [
+         "ec2:DescribeInstances", "ec2:DescribeImages",
+         "ec2:DescribeTags", "ec2:DescribeSnapshots"
+      ],
+      "Resource": "*"
+   }
+   ]
+}
+EOF
 }
 
 /**
